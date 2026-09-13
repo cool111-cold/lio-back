668474f9f84319fc64f89936263b389af04a4b08
Писарев Вадим Сергеевич <v.pisarev@els24.com>
2026-09-13 21:43:10 +0700

    test commit

--- main.py
+++ main.py
@@ -4,8 +4,9 @@ app = FastAPI()
 
 # uvicorn main:app --reload
 # fastapi dev main.py
+# uvicorn main:app --host 0.0.0.0 --port 10000
 
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
+    return {"message": "Hello World 2"}
 
